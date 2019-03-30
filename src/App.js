ef2ea35402981a00671ae0ada7fe05920fa96432
Sahil Chawla <sahil.chawla@clutonik.com>
2019-03-30 17:57:33 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This file has been changed after docker-compose.
           </p>
           <a
             className="App-link"
